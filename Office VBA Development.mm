fe8f8a30a89e1a602bf1067305d516ee9b416ec1
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-10-08 10:10:19 -0400

    Create Office VBA Development.mm

---
+++ Office VBA Development.mm
@@ -0,0 +1,268 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Office VBA Development" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1696773647593"><hook NAME="MapStyle" background="#f5f5dc">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_sepia_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.75 pt;-10.5 pt;" ENDINCLINATION="81.75 pt;19.5 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_411331447" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_411331447"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#2c2b29" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook URI="../../../C:/Users/v0cn037/Pictures/202310/Office-VBA-Middle-Level.png" SIZE="1.0" NAME="ExternalObject"/>
+<richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      作者：刘永富，刘行
+    </p>
+    <p>
+      出版社：清华大学出版社
+    </p>
+    <p>
+      出版时间：2019年6月
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="01 Read/Write Text File&#xa;文本文件的读写" POSITION="bottom_or_right" ID="ID_836146301" CREATED="1696773134261" MODIFIED="1696773678372"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍文本文件的多种读写方式、路径和文件的自动化处理
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="02 Add/Delete Files, Path&#xa;文件、路径的增删" POSITION="bottom_or_right" ID="ID_1319735816" CREATED="1696773155283" MODIFIED="1696773692717"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍自动启动其他应用程序的方法、注册表的读写、环境变量的读写方法
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="03 Read/Write Registry&#xa;注册表的读写" POSITION="bottom_or_right" ID="ID_760641803" CREATED="1696773186178" MODIFIED="1696773709277"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍用WinRAR和Shell32自动处理压缩文件，以压缩文件的视角认识Office文档
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="04 Compress and Decompress&#xa;压缩和解压缩" POSITION="bottom_or_right" ID="ID_656378260" CREATED="1696773200300" MODIFIED="1696774067653"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍XML的语法规则，使用XML DOM对象读写XML文件，XML文件的验证等。该章是Office自定义界面的理论基础
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="05 XML and Office Interface&#xa;XML与Office的界面" POSITION="bottom_or_right" ID="ID_1938268948" CREATED="1696773218881" MODIFIED="1696774079028"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍Office界面五大功能区的customUI技术，详细讲解了用于定制Office界面各种控件的技术要点
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="06 Processing Strings using Regular Expression&#xa;正则表达式处理字符串" POSITION="bottom_or_right" ID="ID_687847037" CREATED="1696773242856" MODIFIED="1696774087249"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍VBA编程中正则表达式的用法，通过丰富的实例演示和体现了正则表达式在VBA编程中的强大之处
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="07 Feature and Usage of Dictionary&#xa;字典的特色和作用" POSITION="bottom_or_right" ID="ID_1575250148" CREATED="1696773295048" MODIFIED="1696774150062"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍字典的构成和特点，详细讲解键值对的添加、删除、修改、检索的技巧和注意点
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="08 ADO Access Database, SQL Query Statement&#xa;ADO访问数据库、SQL查询语句" POSITION="bottom_or_right" ID="ID_609896451" CREATED="1696773322632" MODIFIED="1696774112129"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍使用ADO技术访问Access、Excel数据库，讲解最常用的SQL语句
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="09 Inter-Access and Communication among Different Office Components&#xa;不同Office组件的互相访问和通信" POSITION="bottom_or_right" ID="ID_1839043997" CREATED="1696773352311" MODIFIED="1696774132348"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍跨Office组件编程，讲解前期绑定和后期绑定的区别，以及不同Office组件之间的互相访问
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="10 Project Reference and External Object&#xa;工程引用与外部对象" POSITION="bottom_or_right" ID="ID_1233277129" CREATED="1696773396904" MODIFIED="1696774170839"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍VBA工程中引用的自动添加和移除，外部对象的创建方法
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="11 Acrobat Software and PDF Document Automation&#xa;Acrobat软件和PDF文档自动化" POSITION="bottom_or_right" ID="ID_768283563" CREATED="1696773421215" MODIFIED="1696774193445"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍使用VBA调用Acrobat对象，对Acrobat软件以及PDF文件进行读写
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="12 Automatically Mail Sending&#xa;自动发送邮件" POSITION="bottom_or_right" ID="ID_869804113" CREATED="1696773447581" MODIFIED="1696774203995"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍邮箱的SMTP配置、使用CDO对象自动创建和发送邮件
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="13 Webpage Automation and Data Retrieving&#xa;网页自动化和网页数据获取" POSITION="bottom_or_right" ID="ID_1652448101" CREATED="1696773470633" MODIFIED="1696774213643"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      介绍HTML DOM读写网页，WebBrowser和Internet Explorer实现网页自动化，以及XMLHTTP和WinHttp实现HTTP请求
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
